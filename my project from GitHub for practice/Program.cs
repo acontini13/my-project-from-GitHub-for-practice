0b938c03e56d8e540fa3bbd32c9deda3d3f9bb0c
Alivia Contini <continam@mail.uc.edu>
2024-04-02 11:53:49 -0400

    Add project files.

---
+++ my project from GitHub for practice/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_project_from_GitHub_for_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am Sentient ");
+        }
+    }
+}
